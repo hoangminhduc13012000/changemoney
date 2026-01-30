12111fbf3b0b37b2c3c08dbbfaae442d44a2d8d9
unknown <chauduongphattien2201@gmail.com>
2026-01-30 10:27:19 +0700

    Updates

--- _next/static/yjYirSJX1gJBlgP-xKcQs/_buildManifest.js
+++ _next/static/yjYirSJX1gJBlgP-xKcQs/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/changemoney//_next/:path+",
+        "destination": "/changemoney/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
