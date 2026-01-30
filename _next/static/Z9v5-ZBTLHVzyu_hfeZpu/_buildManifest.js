9aad5d07620f1e97c860c31f98496b622344e704
unknown <chauduongphattien2201@gmail.com>
2026-01-30 09:52:50 +0700

    Updates

---
+++ _next/static/Z9v5-ZBTLHVzyu_hfeZpu/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/changemoney//_next/:path+",
+        "destination": "/changemoney/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
